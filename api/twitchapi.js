5d5d0d8f3cd3fb905987698a714224a61f5fc7c5
unknown <kdolay@ucsd.edu>
2023-06-06 12:40:14 -0700

    Updated API connectivity to work on the backend, removed secrets from code, put WSS related code into functions

---
+++ api/twitchapi.js
@@ -0,0 +1,43 @@
+const TWITCH_GLOBAL_EMOTES_URL = 'https://api.twitch.tv/helix/chat/emotes/global'
+const TWITCH_VALIDATION_URL = 'https://id.twitch.tv/oauth2/validate'
+const TWITCH_CLIENT_ID = 'bmkhxh3eb8cl8uvtkwm6fbrahzgkdx'
+
+const TWITCH_OAUTH_URL = 'https://id.twitch.tv/oauth2/token'
+
+//TODO: Replace a, b.
+//Function that grabs the emotes from a connected api.
+export function getTwitchEmotes({access_token, a, b}, emoteList, source, callback = () => {}) {
+    fetch(TWITCH_GLOBAL_EMOTES_URL, {
+        headers: {
+            'Authorization': 'Bearer ' + access_token,
+            'Client-Id': TWITCH_CLIENT_ID
+        }
+    })
+    .then(response => response.json())
+    .then(response => {
+        emoteList[source] = response
+        console.log("Initializing Callback")
+        callback()
+    })
+}
+
+//Function that will attempt to connect to twitch.tv's api.
+//TODO: Refactor to be just a connection function => take a lambda that is specific per api
+export function connectTwitch(secret, callback) {
+    fetch(TWITCH_OAUTH_URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: new URLSearchParams({
+            'client_id': TWITCH_CLIENT_ID,
+            'client_secret': secret,
+            'grant_type': 'client_credentials'
+        })
+    })
+    .then(response => response.json())
+    .then(response => {
+        console.log(response)
+        callback(response)
+    })
+}
--- index.js
+++ index.js
@@ -2,50 +2,67 @@ import { WebSocketServer } from "ws"
 import * as http from 'http'
 import express from "express"
 import cors from "cors"
+import { connectTwitch, getTwitchEmotes } from "./api/twitchapi.js"
+import 'dotenv/config'
 
 const AYAYA_URL = "https://play-lh.googleusercontent.com/kTkV3EWtNTDVCzRnUdbI5KdXm6Io-IM4Fb3mDcmX9-EOCEXJxnAxaph_leEn6m61E0I"
 
 const port = 2999
 const app = express()
+
 app.use(cors({
     origin: true
 }))
-app.use(express.static("public"))
 
+app.use(express.static("public"))
 const server = http.createServer(app)
 const wss = new WebSocketServer({ server })
 
-console.log("Server initialized")
+const emotes = {}
 
+console.log("Server initialized")
 let displaySockets = []
 let controllerSockets = []
 
-wss.on("connection", (socket) => {
-    console.log("Connection recieved.")
-    console.log(socket.protocol)
-    socket.send(JSON.stringify({
-        type: "executeAnimation",
-        data: {
-            url: AYAYA_URL,
-            emoteDensity: 5
+console.log("Connecting to APIs")
+connectAPIs(() => {
+    initializeWSS()
+})
+
+function initializeWSS() {
+    console.log(emotes)
+    wss.on("connection", (socket) => {
+        console.log("Connection recieved.")
+        console.log(socket.protocol)
+        socket.send(JSON.stringify({
+            type: "executeAnimation",
+            data: {
+                url: AYAYA_URL,
+                emoteDensity: 5
+            }
+        }))
+
+        switch(socket.protocol){
+            case("streamDisplay"):
+                displaySockets.push(socket)
+                console.log(displaySockets)
+                break;
+            case("streamerController"):
+                controllerSockets.push(socket)
+                const data = {
+                    type: "recievedEmotes",
+                    data: emotes
+                }
+                socket.send(JSON.stringify(data))
+                controllerSocketConnection(socket)
+                break;
         }
-    }))
 
-    switch(socket.protocol){
-        case("streamDisplay"):
-            displaySockets.push(socket)
-            console.log(displaySockets)
-            break;
-        case("streamerController"):
-            controllerSockets.push(socket)
-            controllerSocketConnection(socket)
-            break;
-    }
-
-})
-server.listen(port, () => {
-    console.log(`Server started on port ${server.address().port}`);
-})
+    })
+    server.listen(port, () => {
+        console.log(`Server started on port ${server.address().port}`);
+    })
+}
 
 function controllerSocketConnection(socket) {
     socket.on("message", (message) => {
@@ -72,5 +89,15 @@ function controllerSocketConnection(socket) {
             default:
                 break;
         }
+    })
+}
+
+/*TODO: Think about hashing the objects such that we can check if
+additional emotes have been added or modified, and update client based on that
+information.
+*/
+async function connectAPIs(callback) {
+    connectTwitch(process.env['TWITCH_API_SECRET'], (response) => {
+        getTwitchEmotes(response, emotes, "Twitch.tv Global", callback)
     })
 }
